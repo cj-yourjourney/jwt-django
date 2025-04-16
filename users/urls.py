ebd19c7245bdc565274e4b43c9bcb81785c79d79
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-04-16 11:20:24 -0700

    cleaned up the urls

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 from .views import user_details
 
 urlpatterns = [
-    
+
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("user/details/", user_details, name="user_details"),
